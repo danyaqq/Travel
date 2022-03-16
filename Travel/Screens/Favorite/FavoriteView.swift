070ef1f8f55cc1d30f3495206186c94761de28cc
danyaqq <fedak220@gmail.com>
2022-03-16 22:17:41 +0300

    Add favorite view

---
+++ Travel/Screens/Favorite/FavoriteView.swift
@@ -0,0 +1,20 @@
+//
+//  FavoriteView.swift
+//  Travel
+//
+//  Created by Даня on 16.03.2022.
+//
+
+import SwiftUI
+
+struct FavoriteView: View {
+    var body: some View {
+        Text("Favorite")
+    }
+}
+
+struct FavoriteView_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteView()
+    }
+}
